9bd6af262d0442b2ce1f134db7ad806931b3b542
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-03-31 09:11:45 +0300

    My new commit

--- app/src/main/java/fi/oamk/gitdemo/MainActivity.kt
+++ app/src/main/java/fi/oamk/gitdemo/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val foo = 1
+        val foo2 = 2
     }
 }
